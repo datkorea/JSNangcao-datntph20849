101e5cb662600ed0a0e5e7b7819b75d8c9fb4223
datkorea <107116150+datkorea@users.noreply.github.com>
2023-01-07 05:01:25 +0700

    Add files via upload

--- challenge1.js
+++ challenge1.js
@@ -1,70 +1,11 @@
-const game = {
-    team1: 'bayern munich',
-    team2: ' borrussia dortmund',
-    players : [
-        [
-            'neuer',
-            'pavard',
-            'martinez',
-            'alaba',
-            'davies',
-            'kimmich',
-            'goretzka',
-            'coman',
-            'muller',
-            'gnarby',
-            'lewandowsky',
-        ],
-        [
-            'burky',
-            'schulz',
-            'hummels',
-            'akanji',
-            'hakimi',
-            'weigl',
-            'witsel',
-            'hazard',
-            'brandt',
-            'sancho',
-            'gotze',
-        ],
-    ],
-    score: '4:0',
-    scored: ['Lewandowsky','gnarby','Lewandowsky','hummels'],
-    date : 'nov 9th, 2037',
-    odds:{
-        team1:11.33,
-        x:3.25,
-        team2:6.5,
-    },
-};
-//1
-const [players1,players2]= game.players;
-console.log(players1,players2);
-//2
-const [gk, ...fieldPlayers] = players1;
-console.log(gk,fieldPlayers);
-//3
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
-//4
-const players1Final = [...players1,'Thiago','coutinho','periscic'];
-//5
-const {
-    odds: {team1, x:draw, team2},
-
-}=game;
-console.log(team1,draw,team2);
-//6
-const printgoals = function(...players){
-    console.log(`${players.length} goals were scored`);
-};
-
-// printgoals('davies','muller','lewandowsky','kimmich');
-// printgoals('davies','muller');
-printgoals(game.scored);
+const massMark = 95;
+const heightMark=  1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
 
 
-//7
-team1 <  team2 &&  console.log('team 1 is more likely to win');
-team1 >  team2 &&  console.log('team 2 is more likely to win');
+const BMIMark= massMark / heightMark **2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark,BMIJohn,markHigherBMI);
